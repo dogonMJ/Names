ca5e7187e32fa52560a1fb9d56bf9d278692dc2e
dogonMJ <62416558+dogonMJ@users.noreply.github.com>
2020-09-06 14:48:58 +0800

    no message

--- name.R
+++ name.R
@@ -1,20 +1,21 @@
 library(dplyr)
 setwd('C:/Users/user/Desktop/Names')
-df <- read.table('names.txt',sep = "\t",header = T)
+df <- read.table('names_500m.txt',sep = "\t",header = T)
 cat <- c('坡','圳','埤','溝','灣','坑','山','崙','湖','江','頂','湳','溪','潭','水','坪','崎','崁','谷','埔','墘')
 
 res <- vector()
-for (k in c(4)){
+for (k in c(13)){
   plot.new()
   for (i in c(1:21)){
     zz <- filter(df,grepl(cat[i],df$category1))
-    dens <- density(zz[,k])
+    #dens <- density(zz[,k])
     par(new=TRUE)
-    plot(ecdf(zz[,k]),verticals=T,do.points=F,main = colnames(zz)[k],xlim = c(0,2000),ylim = c(0,1))
+    plot(ecdf(zz[,k]),verticals=T,do.points=F,main = colnames(zz)[k],xlim = c(0,200),ylim = c(0,1))
     # plot(dens$x,dens$y,type="l",xlab="Value",ylab="Count estimate",
     #      xlim = c(-200,1000),ylim = c(0,0.005),main = colnames(zz)[k])
-    #res <- rbind(res,c(cat[i],as.numeric(sapply(zz[4:10],mean))))
+    #res <- rbind(res,c(cat[i],as.numeric(sapply(zz[4:13],median))))
   }
 }
-write.table(res,'C:/Users/user/Desktop/mean.txt',sep = '\t',col.names = F)
+colnames(res) <- c('cat',colnames(df[4:13]))
+write.table(res,'median_500m.txt',sep = '\t',col.names = T)
 
