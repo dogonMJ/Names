1d18e8404a9f6f6c8c345546e713ed47a7103305
Guandu-PC\Guandu <b99208035@ntu.edu.tw>
2020-09-18 14:58:42 +0800

    colors using continuous data instead of order add ggplot2 plot

--- name.R
+++ name.R
@@ -8,8 +8,9 @@ res <- vector()
 for (i in c(1:21)){
   #zz <- filter(df,grepl(cat[i],df$category1)) #multiple words included
   zz <- filter(df,df$category1==cat[i]) #if only the word included
-  res <- rbind(res,c(cat[i],as.numeric(sapply(zz[4:13],mean,na.rm = TRUE))))
+  res <- rbind(res,c(as.numeric(sapply(zz[4:13],mean,na.rm = TRUE))))
 }
+res <- data.frame(cat,res)
 colnames(res) <- c('cat',colnames(df[4:13]))
 #write.table(res,'mean_500.txt',sep = '\t',col.names = T)
 
@@ -28,16 +29,27 @@ for (k in c(4:13)){
 }
 
 
-mycolors <- heat.colors(21)
-ord <- rank(-as.numeric(res[,8]))
+# mycolors <- heat.colors(21)
+# ord <- rank(-as.numeric(res[,8]))
+# mycolors <- mycolors[ord]
+mycolors <- (heat.colors(100))
+ord <- round(100*as.numeric(res[,8])/max(as.numeric(res[,8])))
 mycolors <- mycolors[ord]
 
 symbols(res[,5],res[,11],circles = res[,9],fg="grey",bg=mycolors,
-        xlab = colnames(res)[5], ylab = colnames(res)[11], 
-        main = c(colnames(res)[8],'circle size = roughness order; calculated by mean value'))
+        xlab = "距河道平均水平距離(公尺)", ylab = "距河道平均垂直距離(公尺)") #colnames(res)[5]
 text(as.numeric(res[,5]),as.numeric(res[,11]),cat)
 
-#pie(rep(1, 21), col = heat.colors(21), main = "heat") #color wheel
+pie(rep(1, 100), col = heat.colors(100), main = "heat") #color wheel
 
 
+library(ggplot2)
+ggplot(res,aes(x=HDIS_MEAN, y=VDIS_MEAN,label = cat,fill = GRA_MEAN))+
+  geom_point(shape=21, aes(size=ROUGHNESS_MEAN),show.legend = TRUE)+
+  scale_size_continuous(range=c(5,35))+
+  scale_fill_distiller(palette = 'YlOrRd',trans = 'reverse')+
+  geom_text()+
+  xlab('距河道平均水平距離(公尺)')+ylab('距河道平均垂直距離(公尺)')+
+  labs(fill = '坡度(%)', size = '地表粗糙度(-)')+
+  theme_bw()
 
